b20d4f56ca36b84de32ac7c7341a3c7db465bea2
Efe <efecapman@hotmail.com>
2024-04-14 03:09:18 +0300

    attempting to fix test error

--- app/src/main/java/BookManager1/App.java
+++ app/src/main/java/BookManager1/App.java
@@ -1,11 +1,17 @@
 package BookManager1;
 
 public class App {
-    public static void main(String[] args) {
-        // Create an instance of BookManagerFrame1
-        BookManagerFrame1 bookManagerFrame = new BookManagerFrame1();
-        
-        // Set the frame to be visible
-        bookManagerFrame.setVisible(true);
-    }
+  public static void main(String[] args) {
+    // Create an instance of BookManagerFrame1
+    BookManagerFrame1 bookManagerFrame = new BookManagerFrame1();
+    
+    // Set the frame to be visible
+    bookManagerFrame.setVisible(true);
+  }
+
+  // method to test a greeting functionality
+  public String getGreeting() {
+    // Implement logic to return a greeting message
+    return "Hello from Book Catalog!";
+  }
 }
